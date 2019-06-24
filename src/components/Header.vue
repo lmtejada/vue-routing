b1c4a60f527dc79e8a4ae7357d744188626c6769
lmtejada <laura_mtc@hotmail.com>
2019-06-23 20:37:26 -0500

    Routing in a Vue js applicacion - Module

---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+  <ul class="nav nav-pills">
+      <router-link to="/" tag="li" active-class="active" class="nav-item" exact><a>Home</a></router-link>
+      <router-link to="/user" tag="li" active-class="active" class="nav-item"><a>User</a></router-link>
+  </ul>
+</template>
